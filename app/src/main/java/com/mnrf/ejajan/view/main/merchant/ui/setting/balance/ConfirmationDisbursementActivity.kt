a661b4a60d5ebbcabb20eed2217db7f818c7fdf9
ahmadzainulmufid <ahmadzainulmufid27@gmail.com>
2024-11-25 06:11:42 +0700

    slicing ui in merchant setting

---
+++ app/src/main/java/com/mnrf/ejajan/view/main/merchant/ui/setting/balance/ConfirmationDisbursementActivity.kt
@@ -0,0 +1,21 @@
+package com.mnrf.ejajan.view.main.merchant.ui.setting.balance
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.mnrf.ejajan.R
+
+class ConfirmationDisbursementActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_confirmation_disbursement)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
