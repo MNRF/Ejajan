52e3e19c0ee01e75d225f122131458a1c0c4b7eb
ahmadzainulmufid <ahmadzainulmufid27@gmail.com>
2024-11-24 21:46:38 +0700

    slicing ui in merchant menu

---
+++ app/src/main/java/com/mnrf/ejajan/view/main/merchant/ui/menu/add/AddIngredientsActivity.kt
@@ -0,0 +1,21 @@
+package com.mnrf.ejajan.view.main.merchant.ui.menu.add
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.mnrf.ejajan.R
+
+class AddIngredientsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_add_ingredients)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
