cdcc58c76b590072a5e143f5fcbc3ae25ef04ffc
ahmadzainulmufid <ahmadzainulmufid27@gmail.com>
2024-11-25 07:45:16 +0700

    slicing ui for students

---
+++ app/src/main/java/com/mnrf/ejajan/view/main/student/summary/OrderSummaryActivity.kt
@@ -0,0 +1,21 @@
+package com.mnrf.ejajan.view.main.student.summary
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.mnrf.ejajan.R
+
+class OrderSummaryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_order_summary)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
