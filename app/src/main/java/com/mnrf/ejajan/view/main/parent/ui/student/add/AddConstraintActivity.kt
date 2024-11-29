ed25c9c14e52084c9afdf1e5b2aea48297b970b8
mnrf <mnrf29@gmail.com>
2024-11-29 16:49:43 +0700

    Improve UI/UX for splash, onboarding, and login

--- app/src/main/java/com/mnrf/ejajan/view/main/parent/ui/student/add/AddConstraintActivity.kt
+++ app/src/main/java/com/mnrf/ejajan/view/main/parent/ui/student/add/AddConstraintActivity.kt
@@ -2,17 +2,48 @@ package com.mnrf.ejajan.view.main.parent.ui.student.add
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.mnrf.ejajan.R
+import com.mnrf.ejajan.databinding.ActivityParentAddBinding
+import com.mnrf.ejajan.databinding.ActivityParentBinding
 
 class AddConstraintActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityParentAddBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_parent_add)
+        binding = ActivityParentAddBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val allergy = resources.getStringArray(R.array.Allergy)
+
+        if (binding.spConstraint != null) {
+            val adapter = ArrayAdapter(this,
+                android.R.layout.simple_spinner_item, allergy)
+            binding.spConstraint.adapter = adapter
+
+            binding.spConstraint.onItemSelectedListener = object :
+                AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(parent: AdapterView<*>,
+                                            view: View, position: Int, id: Long) {
+                    Toast.makeText(this@AddConstraintActivity,
+                        /*getString(R.string.selected_item) + " " +
+                                "" + */allergy[position], Toast.LENGTH_SHORT).show()
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>) {
+                    // write code to perform some action
+                }
+            }
+        }
 
         val btnTambah = findViewById<Button>(R.id.btn_create)
 
