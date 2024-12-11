a7549d586d2b5dec2aa4ed1c77f970939b0daad7
ahmadzainulmufid <ahmadzainulmufid27@gmail.com>
2024-12-11 16:40:08 +0700

    get order history firestore in role parent menu report

--- app/src/main/java/com/mnrf/ejajan/view/main/parent/ui/adapter/HistoryAdapter.kt
+++ app/src/main/java/com/mnrf/ejajan/view/main/parent/ui/adapter/HistoryAdapter.kt
@@ -1,4 +1,55 @@
 package com.mnrf.ejajan.view.main.parent.ui.adapter
 
-class HistoryAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.mnrf.ejajan.data.model.CartModel
+import com.mnrf.ejajan.databinding.ItemParentHistoryBinding
+
+class HistoryAdapter : ListAdapter<CartModel, HistoryAdapter.MenuViewHolder>(DIFF_CALLBACK) {
+
+    inner class MenuViewHolder(private val binding: ItemParentHistoryBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(cart: CartModel) {
+            binding.apply {
+                Glide.with(binding.placeholderImage.context)
+                    .load(cart.imageurl)
+                    .into(binding.placeholderImage)
+                tvPlaceholderName.text = cart.name
+                val cartPrice = "Rp.${cart.price}"
+                val quantity = cart.quantity
+                tvPlaceholderPrice.text = cartPrice
+                tvPlaceholderQuantity.text = quantity
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MenuViewHolder {
+        val binding = ItemParentHistoryBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+        return MenuViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: MenuViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    companion object {
+        private val DIFF_CALLBACK = object : DiffUtil.ItemCallback<CartModel>() {
+            override fun areItemsTheSame(oldItem: CartModel, newItem: CartModel): Boolean {
+                return oldItem.name == newItem.name
+            }
+
+            override fun areContentsTheSame(oldItem: CartModel, newItem: CartModel): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
 }
--- app/src/main/java/com/mnrf/ejajan/view/main/parent/ui/report/ReportViewModel.kt
+++ app/src/main/java/com/mnrf/ejajan/view/main/parent/ui/report/ReportViewModel.kt
@@ -1,13 +1,62 @@
 package com.mnrf.ejajan.view.main.parent.ui.report
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.mnrf.ejajan.data.model.CartModel
+import com.mnrf.ejajan.data.model.UserModel
+import com.mnrf.ejajan.data.repository.UserRepository
 
-class ReportViewModel : ViewModel() {
+class ReportViewModel(private val repository: UserRepository) : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+    private val _isLoading = MutableLiveData<Boolean>()
+    val isLoading: LiveData<Boolean> = _isLoading
+
+    private val _orderList = MutableLiveData<List<CartModel>>()
+    val orderList: LiveData<List<CartModel>> get() = _orderList
+
+    val db = Firebase.firestore
+
+    fun getSession(): LiveData<UserModel> {
+        _isLoading.value = true
+        return repository.getSession().asLiveData()
     }
-    val text: LiveData<String> = _text
+
+    private fun fetchOrderList() {
+        getSession().observeForever { user ->
+            if (user != null) {
+                db.collection("order")
+                    .get()
+                    .addOnSuccessListener { result ->
+                        val order = result.map { document ->
+                            CartModel(
+                                id = document.id,
+                                name = document.getString("menu_name") ?: "",
+                                price = document.getString("menu_price") ?: "",
+                                quantity = document.getString("menu_quantity") ?: "",
+                                imageurl = document.getString("menu_imageurl") ?: "",
+                            )
+                        }
+                        _orderList.value = order
+                        _isLoading.value = false
+                    }
+                    .addOnFailureListener { exception ->
+                        Log.e(TAG, "Error fetching menu list", exception)
+                        _isLoading.value = false
+                    }
+            }
+        }
+    }
+
+    init {
+        fetchOrderList()
+    }
+
+    companion object {
+        const val TAG = "ReportViewModel"
+    }
 }
