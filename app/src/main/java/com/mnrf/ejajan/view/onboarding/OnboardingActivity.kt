24fc8653108f0b56cc075aeb2ec8c608a211e11e
ahmadzainulmufid <ahmadzainulmufid27@gmail.com>
2024-12-11 14:13:28 +0700

    add animation

--- app/src/main/java/com/mnrf/ejajan/view/onboarding/OnboardingActivity.kt
+++ app/src/main/java/com/mnrf/ejajan/view/onboarding/OnboardingActivity.kt
@@ -1,7 +1,10 @@
 package com.mnrf.ejajan.view.onboarding
 
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.mnrf.ejajan.databinding.ActivityOnboardingBinding
 
@@ -13,6 +16,8 @@ class OnboardingActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         supportActionBar?.hide()
+
+        playAnimation()
 
         binding = ActivityOnboardingBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -34,5 +39,24 @@ class OnboardingActivity : AppCompatActivity() {
         val intent = Intent(this, OnboardingViewPager::class.java)
         intent.putExtra("ROLE_KEY", role)
         startActivity(intent)
+    }
+
+    private fun playAnimation() {
+        ObjectAnimator.ofFloat(binding.choseeRole, View.TRANSLATION_X, -30f, 30f).apply {
+            duration = 6000
+            repeatCount = ObjectAnimator.INFINITE
+            repeatMode = ObjectAnimator.REVERSE
+        }.start()
+
+        val welcome = ObjectAnimator.ofFloat(binding.welcomeText, View.ALPHA, 1f).setDuration(100)
+        val role = ObjectAnimator.ofFloat(binding.roleText, View.ALPHA, 1f).setDuration(100)
+        val btnStudent = ObjectAnimator.ofFloat(binding.btnRoleStudent, View.ALPHA, 1f).setDuration(100)
+        val btnParent = ObjectAnimator.ofFloat(binding.btnRoleParent, View.ALPHA, 1f).setDuration(100)
+        val btnMerchant = ObjectAnimator.ofFloat(binding.btnRoleMerchant, View.ALPHA, 1f).setDuration(100)
+
+        AnimatorSet().apply {
+            playSequentially(welcome, role, btnStudent, btnParent, btnMerchant)
+            start()
+        }
     }
 }
